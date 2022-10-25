89f3698177ea618aadef27cd1ac7e626353b46cd
AJ Igherighe <the.coders.dao@gmail.com>
2022-10-24 20:51:32 -0400

    added router and router types

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,19 @@ import { AddressForm } from "./AddressForm";
 import { useMultistepForm } from "./useMultistepForm";
 import { UserForm } from "./UserForm";
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Home />
+  },
+])
+
 
 type FormData = {
   firstName: string;
@@ -54,46 +67,64 @@ function App() {
   }
 
   return (
+    <>
+      <Home onSubmit={onSubmit} currentStepIndex={currentStepIndex} step={step} 
+      steps={steps} isFirstStep={isFirstStep} back={back} isLastStep={isLastStep}
+      />
+    </>
+  );
+}
+
+function Home({onSubmit, currentStepIndex, steps, step, isFirstStep, back, isLastStep}:{
+  onSubmit:any,
+  currentStepIndex:any,
+  steps: any,
+  step: any,
+  isFirstStep: boolean,
+  back:any,
+  isLastStep:boolean
+  }) {
+  return (
     <div className="container grid">
-      <div className="row">
-        <div className="twelve columns">
-          <form onSubmit={onSubmit}>
-            <div className="row">
-              <div className="ten columns">
-                <span style={{ color: "white" }}>asdlkfjl</span>
-              </div>
-              <div className="one column">
-                <span style={{ color: "black", font: "bold" }}>
-                  {currentStepIndex + 1}
-                </span>{" "}
-                / <span>{steps.length}</span>
-              </div>
+    <div className="row">
+      <div className="twelve columns">
+        <form onSubmit={onSubmit}>
+          <div className="row">
+            <div className="ten columns">
+              <span style={{ color: "white" }}>asdlkfjl</span>
             </div>
-            {step}
-            <div className="row">
-              <div className="nine columns">
-                <span style={{ color: "white" }}>aslfjal</span>
-              </div>
-              <div>
-                <span style={{ margin: "0 0.5rem" }}>
-                  {!isFirstStep && (
-                    <button type="button" onClick={back}>
-                      Back
-                    </button>
-                  )}
-                </span>
-                <span style={{ margin: "0 0.5rem" }}>
-                  <button type="submit">
-                    {isLastStep ? "Finish" : "Next"}
+            <div className="one column">
+              <span style={{ color: "black", font: "bold" }}>
+                {currentStepIndex + 1}
+              </span>{" "}
+              / <span>{steps.length}</span>
+            </div>
+          </div>
+          {step}
+          <div className="row">
+            <div className="nine columns">
+              <span style={{ color: "white" }}>aslfjal</span>
+            </div>
+            <div>
+              <span style={{ margin: "0 0.5rem" }}>
+                {!isFirstStep && (
+                  <button type="button" onClick={back}>
+                    Back
                   </button>
-                </span>
-              </div>
+                )}
+              </span>
+              <span style={{ margin: "0 0.5rem" }}>
+                <button type="submit">
+                  {isLastStep ? "Finish" : "Next"}
+                </button>
+              </span>
             </div>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
     </div>
-  );
+  </div>
+  )
 }
 
 export default App;
