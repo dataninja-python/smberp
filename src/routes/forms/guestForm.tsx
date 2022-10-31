8146e365548e3781ab946f5cf88cbca1ba9f4deb
AJ Igherighe <the.coders.dao@gmail.com>
2022-10-31 11:34:35 -0400

    updated forms

--- src/routes/forms/guestForm.tsx
+++ src/routes/forms/guestForm.tsx
@@ -10,8 +10,19 @@ import BackBtn from './components/backBtn';
 import NextBtn from './components/nextBtn';
 
 import GuestForm1 from './sub-forms/form1'
+import GuestForm2 from './sub-forms/form2'
+import GuestForm3 from './sub-forms/form3'
+import GuestForm4 from './sub-forms/form4'
 
 export default function GuestForms() {
+  const initPage = useGuestStore((state:any) => state.initPageNum);
+  let currentPage: number = useGuestStore((state:any) => state.curentPage);
+  const [index, setIndex] = React.useState(initPage - 1)
+  
+  React.useEffect(() => {
+    setIndex((currentPage - 1)
+  }, [currentPage])
+
   return (
     <>
     <div className="container">
